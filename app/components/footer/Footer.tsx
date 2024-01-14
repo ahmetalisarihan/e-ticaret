c8a6cd505cd29adde985320ef59d7a256c0205b6
ahmetalisarihan <ahmetalisarihan@gmail.com>
2024-01-14 23:50:54 +0300

    Add footer

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="h-34 bg-sky-500 border-t border-peach max-width">
+      <div className="flex items-center justify-center mt-3 md:mt-10">Düzenlenecek...</div>
 
-export default Footer
+      {/* Copyright text */}
+      <p className="mt-8 text-center text-grey-600 text-sm">
+        Copyright © {new Date().getFullYear()} AAS. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
