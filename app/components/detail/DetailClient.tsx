73bfeabf6531710bbc70cff62d41afdea86cc00b
ahmetalisarihan <ahmetalisarihan@gmail.com>
2024-01-16 23:03:16 +0300

    Add button component

--- app/components/detail/DetailClient.tsx
+++ app/components/detail/DetailClient.tsx
@@ -3,6 +3,10 @@ import React, { useState } from "react";
 import PageContainer from "../containers/PageContainer";
 import Image from "next/image";
 import Counter from "../general/Counter";
+import { Rating } from "@mui/material";
+import Button from "../general/Button";
+import Comment from "./Comment";
+import Heading from "../general/Heading";
 
 
 export type CardProductProps = {
@@ -37,24 +41,35 @@ const DetailClient = ({ product }: { product: any }) => {
          if(cardProduct.quantity == 1) return
          setCardProduct(prev => ({...prev, quantity: prev.quantity - 1 }))
      }
-
+     let productRating = product?.reviews?.reduce((acc: number, item: any) => acc + item.rating, 0) / product?.reviews?.length
   return (
     <div className="my-10">
       <PageContainer>
         <div className="block md:flex gap-10 justify-center">
-          <div className="relative h-[400px] w-[400px]">
+          <div className="relative h-[200px] md:h-[400px] w-[200px] md:w-[400px] mb-3 md:mb-0">
             <Image src={product?.image} alt="" fill />
           </div>
           <div className="w-1/2 space-y-3">
             <div className="text-xl md:text-2xl">{product?.name}</div>
+            <Rating name="read-only" value={productRating} readOnly />
             <div className="text-slate-500">{product?.description}</div>
             <div className="flex items-center gap-2">
             <div>STOK DURUMU:</div>
             {product.inStock ? <div className="text-green-500">Stokta var</div> : <div className="text-red-500">Stokta yok</div>}
           </div>
           <Counter increaseFunc={increaseFunc} decreaseFunc={decreaseFunc} cardProduct={cardProduct} />
+          <div className="text-lg md:text-2xl text-orange-600 font-bold">{product.price} ₺</div>
+          <Button text="Sepete Ekle" small onClick={() => console.log(cardProduct)} />
           </div>
           
+        </div>
+        <Heading  text="Yorumlar" />
+        <div>
+        {
+                    product?.reviews?.map((prd: any) => (
+                        <Comment key={prd.id} prd={prd}/>
+                    ))
+                }
         </div>
       </PageContainer>
     </div>
