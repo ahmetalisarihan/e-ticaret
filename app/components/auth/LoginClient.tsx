8dedeb1f4f05921070ab25b2389c0c8ddb7d02dd
ahmetalisarihan <ahmetalisarihan@gmail.com>
2024-01-20 23:08:04 +0300

    Add authentication login

--- app/components/auth/LoginClient.tsx
+++ app/components/auth/LoginClient.tsx
@@ -1,9 +1,33 @@
+"use client"
 import React from 'react'
 import AuthContainer from '../containers/AuthContainer'
+import Heading from '../general/Heading'
+import Input from '../general/Input'
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+import { useRouter } from 'next/navigation'
+import Button from '../general/Button'
+import { FaGoogle } from 'react-icons/fa'
+import Link from 'next/link'
 
 const LoginClient = () => {
+    const router = useRouter();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        console.log(data);
+    }
   return (
-    <AuthContainer>Login</AuthContainer>
+    <AuthContainer>
+        <div className="w-full md:w-[500px] p-3 shadow-lg rounded-md">
+        <Heading text="Login" center/>
+        <Input placeholder="Email" type="text" id="email" register={register} errors={errors} required/>
+        <Input placeholder="Parola" type="password" id="password" register={register} errors={errors} required/>
+        <Button text="Giriş Yap" onClick={handleSubmit(onSubmit)}/>
+        <div className="text-center my-2 text-sm text-red-500">Daha Önce Kayıt Olmadıysanız <Link className="underline " href="/register">buraya tıklayınız</Link></div>
+              <div className="text-center my-2 font-bold text-lg">OR</div>
+              <Button text="Google İle Giriş Yap" icon={FaGoogle} outline  onClick={() => {}}/>
+        </div>
+    </AuthContainer>
   )
 }
 
