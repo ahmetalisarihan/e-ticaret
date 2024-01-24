617b35aa191928e71dd48d949f88a58f2b2bb315
ahmetalisarihan <105447763+ahmetalisarihan@users.noreply.github.com>
2024-01-24 23:56:56 +0300

    Add admin sidebar

---
+++ app/admin/layout.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react'
+import AdminSidebar from '../components/admin/AdminSidebar'
+
+const AdminLayout = ({children}:{children:React.ReactNode}) => {
+  return (
+    <div className='flex gap-3'>
+        <AdminSidebar />
+        {children}
+    </div>
+  )
+}
+
+export default AdminLayout
